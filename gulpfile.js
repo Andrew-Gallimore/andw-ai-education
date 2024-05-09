0c2bcc8ed367047a3468e286fdea86a06c29982a
Andrew Gallimore <gallimoreandrew04@gmail.com>
2024-05-09 09:19:52 -0700

    Fixed the gulp src/href prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const replace = require('gulp-replace');
 // Prefixes all src="" attributes in the index.html file with /projects/ai-education/
 gulp.task('prefix-src', function(){
     return gulp.src('src/index.html')
-        .pipe(replace(/src="([^"]*)"/g, 'src="/projects/ai-education/$1"'))
+        .pipe(replace(/src="(?!https:\/\/)([^"]*)"/g, 'src="/projects/ai-education/$1"'))
+        .pipe(replace(/href="(?!https:\/\/)([^"]*)"/g, 'href="/projects/ai-education/$1"'))
         .pipe(gulp.dest('dist'));
 });
 
